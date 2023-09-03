883b5801d95e33f6f59a7cd73e3212e1e294c50c
henriquecpp <henriquedotcpp@gmail.com>
2023-09-03 18:45:30 -0300

    [feat] - funcao para exibir o popup de erro ao ocorrer alguma falha durante o processamento

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
 import { Observable, catchError, of } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from '../../shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -13,7 +15,7 @@ export class CoursesComponent implements OnInit {
   courses$: Observable<Course[]> = new Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
-  constructor(private coursesService: CoursesService) {
+  constructor(private coursesService: CoursesService, public dialog: MatDialog) {
     //this.coursesService = new CoursesService();
   }
 
@@ -21,9 +23,16 @@ export class CoursesComponent implements OnInit {
     this.courses$ = this.coursesService.list()
     .pipe(
       catchError(error => {
+        this.openDialog('Não foi possível carregar os dados');
         return of([])
       }),
     );
+  }
+
+  openDialog(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
   }
 
 }
