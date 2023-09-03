6543849cddbe8de7f31a66495e02f78cb1ba2776
henriquecpp <henriquedotcpp@gmail.com>
2023-09-03 19:34:00 -0300

    [chore] - exportando Dialog e Button module

--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -3,14 +3,18 @@ import { MatTableModule } from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
-
+//Modulo usado para centralizar os modulos do angular material, servindo como um 'Módulo Pai'
 @NgModule({
   exports: [
     MatTableModule,
     MatCardModule,
     MatToolbarModule,
     MatProgressSpinnerModule,
+    MatDialogModule,
+    MatButtonModule,
   ],
 })
 export class AppMaterialModule { }
