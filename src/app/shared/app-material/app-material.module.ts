da7afea3c9117bcbf53ba8ca928e3cb0983fc4f9
henriquecpp <henriquedotcpp@gmail.com>
2023-09-17 18:53:17 -0300

    [chore] - importando o MatInput

--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -7,6 +7,7 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 //Modulo usado para centralizar os modulos do angular material, servindo como um 'Módulo Pai'
 @NgModule({
@@ -19,6 +20,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatButtonModule,
     MatIconModule,
     MatFormFieldModule,
+    MatInputModule,
   ],
 })
 export class AppMaterialModule { }
