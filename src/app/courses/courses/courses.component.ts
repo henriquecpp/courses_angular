cf06a7d4ccf6ca55bd047f3c3252f78bc765ff24
henriquecpp <henriquedotcpp@gmail.com>
2023-10-15 16:21:59 -0300

    [refactor] - ajuste de imports

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
 import { Observable, catchError, of } from 'rxjs';
-import { MatLegacyDialog as MatDialog } from '@angular/material/legacy-dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { ErrorDialogComponent } from '../../shared/components/error-dialog/error-dialog.component';
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -14,13 +14,15 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class CoursesComponent implements OnInit {
 
   courses$: Observable<Course[]> = new Observable<Course[]>;
-  displayedColumns = ['name', 'category', 'actions'];
+  displayedColumns: string[] = ['name', 'category', 'actions'];
 
   constructor(private coursesService: CoursesService, public dialog: MatDialog, private router: Router, private route: ActivatedRoute) {
     //this.coursesService = new CoursesService();
   }
 
   onAdd() {
+    console.log(true);
+
     this.router.navigate(['new'], { relativeTo: this.route });
   }
 
